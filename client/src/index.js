93f47de5a495c183cd4c5ec592bb2956efbbf744
seonhwa oh <seonhwa_0713@naver.com>
2021-10-25 10:53:59 +0900

    Feat:home page 레이아웃

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import store from "./store/store"
 import { Provider } from "react-redux"
+import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
-    <React.StrictMode>
+    <BrowserRouter>
+      <React.StrictMode>
         <Provider store={store}>
             <App />
         </Provider>
-    </React.StrictMode>,
+      </React.StrictMode>
+    </BrowserRouter>,
     document.getElementById("root")
 )
