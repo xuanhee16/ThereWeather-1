95771c832d91c57bf49d05e3bdfd4373568a255a
이상아 <lebikebihebi@gmail.com>
2021-10-25 22:35:49 +0900

    Feat: 비밀번호 수정 페이지- 회원탈퇴 모달 동작 확인

--- client/src/components/ModalConfirm.js
+++ client/src/components/ModalConfirm.js
@@ -4,12 +4,16 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 
 const Outer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100vw;
   height: 100vh;
   background-color: var(--modal-bg-color);
+  z-index: 200;
 `;
 
 const Popup = styled.section`
@@ -28,7 +32,7 @@ const Popup = styled.section`
   border-radius: 1.5rem;
 
   & > article {
-    margin: 1.5rem;
+    margin: .5rem;
   }
 
   & .closeButtonArea {
@@ -68,3 +72,28 @@ export default function ModalConfirm({ children }) {
     </Outer>
   );
 }
+
+/* 사용 예시
+- props
+  - isOpen
+    - true 할당 -> 보인다
+    - isOpen을 쓰지 않거나 false 할당 -> 안 보인다
+(1) 삭제
+  <ModalConfirm>삭제하시겠습니까?</ModalConfirm>
+
+(2) 수정
+  <ModalConfirm>수정하시겠습니까?</ModalConfirm>
+
+(3) 탈퇴
+  <ModalConfirm>
+    <p>탈퇴하시겠습니까?</p>
+    <p>이유를 선택해 주세요</p>
+    <select name="pets" id="pet-select">
+      <option value="">선택</option>
+      <option value="notUseful">사용을 많이 하지 않음</option>
+      <option value="inconvenientDesign">디자인이 불편함</option>
+      <option value="otherOptions">다른 앱을 이용하기 위해</option>
+      <option value="andSoForth">기타</option>
+    </select>
+  </ModalConfirm>
+*/
