92ef10b61af8bc4db671c3b4a84d91ff07b84eba
seonhwa oh <seonhwa_0713@naver.com>
2021-10-26 20:26:36 +0900

    Feat:pages/BookMark.js 페이지네이션 수정중

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,4 +1,5 @@
 import { CHANGE_USER_INFO } from "../actions/index"
+import { UPDATE_CURRENT_PAGE, UPDATE_START_END_PAGE } from "../actions/index"
 import { initialState } from "./initialState"
 
 const itemReducer = (state = initialState, action) => {
@@ -9,11 +10,21 @@ const itemReducer = (state = initialState, action) => {
                 userInfo: action.payload,
             }
 
-            break
+        //     break
         // 새로운기능은 아래양식으로 만들어서 쓸수있다.
         // case CHANGE_USER_INFO:
         //     break
-
+        case UPDATE_CURRENT_PAGE: 
+          return {
+              ...state,
+              current: action.payload
+          } 
+        case UPDATE_START_END_PAGE:
+            return {
+                ...state, 
+                start: action.payload.start,
+                end: action.payload.end
+            }
         default:
             return state
     }
