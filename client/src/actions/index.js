e2fa159f53f915897cb81b9b6a37ae9188660f18
이상아 <lebikebihebi@gmail.com>
2021-10-28 11:40:15 +0900

    Design: 파비콘 추가

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,23 +3,17 @@ import { useHistory } from "react-router-dom"
 
 // action types
 export const CHANGE_USER_INFO = "CHANGE_USER_INFO"
-<<<<<<< HEAD
 export const UPDATE_CURRENT_PAGE = "UPDATE_CURRENT_PAGE"
 export const UPDATE_START_END_PAGE = "UPDATE_START_END_PAGE"
 
 export const SIGNIN_USER = "SIGNIN_USER"
 export const LOGGEDIN_USER = "LOGGEDIN_USER"
 export const SIGNUP_USER = "SIGNUP_USER"
-=======
 export const CHANGE_USER_GENDER = "CHANGE_USER_GENDER"
-export const UPDATE_CURRENT_PAGE = "UPDATE_CURRENT_PAGE"
-export const UPDATE_START_END_PAGE = "UPDATE_START_END_PAGE"
 export const CHANGE_IS_LOGIN = "CHANGE_IS_LOGIN"
 
 // export const LOGIN_USER = "LOGIN_USER"
->>>>>>> a23b0cdc3636df9ecea569a78487b1c87f2f089a
 
-export const CHANGE_USER_GENDER = "CHANGE_USER_GENDER"
 
 
 // actions creator functions
@@ -48,7 +42,6 @@ export const updateStartEndPage = (start, end) => {
         },
     }
 }
-<<<<<<< HEAD
 
 //홈 바로가기
 export const Home = (history) => () => {
@@ -97,7 +90,6 @@ export const signUpUser = (data, history) => (dispatch) => {
 }
 
 
-=======
 // export const loginUser = (data) => {
 //   const loginDate = axios.post("http://localhost:3000/login", data)
 //   .then(res => res.data)
@@ -106,7 +98,6 @@ export const signUpUser = (data, history) => (dispatch) => {
 //       payload: loginDate
 //   }
 // }
->>>>>>> a23b0cdc3636df9ecea569a78487b1c87f2f089a
 
 export const changeGender = (usergender) => {
     return {
--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import { CHANGE_USER_INFO, CHANGE_USER_GENDER } from "../actions/index"
-import { UPDATE_CURRENT_PAGE, UPDATE_START_END_PAGE, LOGGEDIN_USER, SIGNIN_USER, SIGNUP_USER } from "../actions/index"
-=======
-import { CHANGE_USER_INFO, CHANGE_USER_GENDER, CHANGE_IS_LOGIN, UPDATE_CURRENT_PAGE, UPDATE_START_END_PAGE, LOGIN_USER } from "../actions/index"
->>>>>>> a23b0cdc3636df9ecea569a78487b1c87f2f089a
+import { LOGGEDIN_USER, SIGNIN_USER, SIGNUP_USER, CHANGE_USER_INFO, CHANGE_USER_GENDER, CHANGE_IS_LOGIN, UPDATE_CURRENT_PAGE, UPDATE_START_END_PAGE, LOGIN_USER } from "../actions/index"
 import { initialState } from "./initialState"
 
 const itemReducer = (state = initialState, action) => {
@@ -29,21 +24,15 @@ const itemReducer = (state = initialState, action) => {
         case UPDATE_CURRENT_PAGE:
             return {
                 ...state,
-<<<<<<< HEAD
                 current: action.payload
             } 
             break;
-=======
-                current: action.payload,
-            }
->>>>>>> a23b0cdc3636df9ecea569a78487b1c87f2f089a
         case UPDATE_START_END_PAGE:
             return {
                 ...state,
                 start: action.payload.start,
                 end: action.payload.end,
             }
-<<<<<<< HEAD
             break;
         case LOGGEDIN_USER:
             return {
@@ -71,7 +60,6 @@ const itemReducer = (state = initialState, action) => {
               }
             }
             break;
-=======
         // case LOGIN_USER:
         //     return {
         //         ...state,
@@ -84,7 +72,6 @@ const itemReducer = (state = initialState, action) => {
                 isLogin: action.payload,
             }
 
->>>>>>> a23b0cdc3636df9ecea569a78487b1c87f2f089a
         default:
             return state
     }
