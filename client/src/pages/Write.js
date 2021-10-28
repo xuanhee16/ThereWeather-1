a600d0ffbb97bdd9e85b0258f5bf2aa0f284745d
이상아 <lebikebihebi@gmail.com>
2021-10-28 20:54:12 +0900

    Design: 사진 업로드 버튼을 원으로

--- client/src/pages/Write.js
+++ client/src/pages/Write.js
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 import { useState, useEffect } from "react"
-import { SunFill, CloudyFill, CloudRainFill, Snow, Thermometer, ThermometerHalf, ThermometerHigh } from "@styled-icons/bootstrap"
+import { Upload, SunFill, CloudyFill, CloudRainFill, Snow, Thermometer, ThermometerHalf, ThermometerHigh } from "@styled-icons/bootstrap"
 
 /*
 import { Wind as Breeze } from "@styled-icons/feather"
@@ -41,10 +41,16 @@ const Outer = styled.div`
 
 const Button = styled.button`
     border: 1px solid black;
+    border-radius: ${props => props.round ? '50%' : null};
     background-color: var(--button-bg-normal);
     font-size: 1.25rem;
-    padding: 0.5rem 2rem;
-    margin: 1rem;
+    padding: ${props => props.round ? '.8rem' : '.8rem 2rem'};
+    margin: ${props => props.round ? '.5rem' : '1rem'};
+
+    & > svg {
+      height: 1.5rem;
+      width: 1.5rem;
+    }
 `
 
 const PictureSection = styled.section`
@@ -170,9 +176,9 @@ export default function Write() {
     realImageNormal: "https://cdn.pixabay.com/photo/2020/11/08/13/28/tree-5723734_1280.jpg",
     realImageLong: "https://cdn.pixabay.com/photo/2021/09/03/02/08/skyscrapers-6594833_1280.png"
   }
-  
+
   // state 변수
-  const [ photoSrc, setPhotoSrc ] = useState(imageUrl.realImageLong);
+  const [ photoSrc, setPhotoSrc ] = useState(imageUrl.realImageNormal);
 
   // 날씨 버튼
     // 날씨 필터링용 state
@@ -275,7 +281,9 @@ export default function Write() {
     <Outer className="writePage">
       <PictureSection className="pictureUploadSection writePageLeft">
         <img src={photoSrc} alt="dummy" />
-        <Button className="uploadButton" onClick={photoUploadButtonHandler}>사진 업로드</Button>
+        <Button className="uploadButton" onClick={photoUploadButtonHandler} round>
+          <Upload />
+        </Button>
       </PictureSection>
 
       <DesktopRight className="writePageRight">
@@ -337,6 +345,3 @@ export default function Write() {
     </Outer>
   );
 }
-
-// dummy text
-// 그들에게 무엇을 피고, 구하기 사는가 두기 운다. 보이는 구하지 있는 얼마나 미인을 철환하였는가? 목숨을 몸이 미묘한 열락의 대중을 청춘 너의 뿐이다. 때에, 사라지지 못할 피부가 인생을 때문이다. 가슴에 것이다.보라, 속에서 주며, 속에 피에 때문이다. 노래하며 소금이라 별과 품에 간에 보이는 대중을 앞이 그리하였는가? 때까지 열매를 위하여 구하기 귀는 것이다. 방지하는 시들어 내려온 못하다 물방아 길을 있을 때문이다. 피고 가진 찾아 꽃이 뭇 인생을 예수는 어디 뿐이다. 구하기 청춘의 불러 같이, 바이며, 이상의 장식하는 청춘 사라지지 칼이다. 역사를 얼마나 넣는 간에 현저하게 풍부하게 얼음에 가는 할지니, 것이다.
