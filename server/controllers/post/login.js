1c25e9c8e8a9c5b4b57e8521d0a344d68c38b2b5
seonhwa oh <seonhwa_0713@naver.com>
2021-10-29 12:20:51 +0900

    Fix:로그인 수정2

--- server/controllers/post/login.js
+++ server/controllers/post/login.js
@@ -6,34 +6,31 @@ module.exports = async (req, res) => {
     //console.log(req.body)
     const { user_id, password } = req.body
 
-    const userInfo = await user.findOne({
-        where: {
-            user_id,
-            // password,
-        },
-    })
-    
+    try{
+      const userInfo = await user.findOne({
+          where: {
+            user_id
+          }
+      })
+      if(!userInfo) {
+      return res.status(401).send('아이디를 확인해주세요.');
+      }
 
-  const dePw = decrypto(userInfo.password);
-    //console.log(dePw)
-    if(dePw !== password){
+      const dePw = decrypto(userInfo.password);
+      //console.log(dePw)
+      if(dePw !== password){
       return res.status(401).send("비밀번호를 확인해주세요.")
       }
-      delete userInfo.dataValues.password;
-      const accessToken = getAccessToken(userInfo.dataValues);
-      const refreshToken = getRefreshToken(userInfo.dataValues);
-      //sendToken(res, accessToken, refreshToken);
-      sendToken(res, accessToken);
-      
-    //비밀번호가 맞지 않음
-    // if (!userInfo) {
-    //     res.status(410).send("일치하는 정보 없음")
-    // } else {
-    //     delete userInfo.dataValues.password
-    //     const accessToken = getAccessToken(userInfo.dataValues)
-    //     const refreshToken = getRefreshToken(userInfo.dataValues)
-    //     sendToken(res, accessToken)
-    //     // sendToken(res, accessToken, refreshToken)
-    // }
+      else{
+        delete userInfo.dataValues.password
+            const accessToken = getAccessToken(userInfo.dataValues)
+            const refreshToken = getRefreshToken(userInfo.dataValues)
+            sendToken(res, accessToken, refreshToken)
+            //sendToken(res, accessToken)
+      }
+    }catch(err){
+        console.log(err)
+    }
 }
+    
 
