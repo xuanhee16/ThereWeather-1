b548d75c06ce3d0206198a69069a91e97764ee2e
root <root@DESKTOP-3C4CCR8.localdomain>
2021-11-01 16:15:57 +0900

    [Clinet/Server]배포테스트 28

--- server/index.js
+++ server/index.js
@@ -49,51 +49,11 @@ app.get("/", (req, res) => {
 })
 
 //겹치는거
-// app.use("/users", upload.single("img"), userRouter)
+app.use("/users", upload.single("img"), userRouter)
 app.post("/sociallogin", controllers.sociallogin) //인증 - App.js
 
 //get
-app.post("/signup", async (req, res) => {
-    console.log("여긴 users/signup/")
-    console.log(req.body)
-    const { user_id, nickName, password } = req.body
-    //id중복검사와 닉네임 중복검사를 서버가 함.
-    //id중복검사
-
-    const user_Id_FindOne = await user.findOne({
-        where: {
-            // user_id: req.body.user_id,
-            user_id,
-        },
-    })
-    const nickName_FindOne = await user.findOne({
-        where: {
-            // nickName: req.body.nickName,
-            nickName,
-        },
-    })
-    if (user_Id_FindOne) {
-        //id중복
-        res.status(211).send("id중복")
-    } else if (nickName_FindOne) {
-        //id중복
-        res.status(212).send("닉네임중복")
-    } else {
-        //정상의경우
-        const enPw = encrypto(password)
-        console.log("중복이 아닙니다.")
-        await user.create({
-            user_id: req.body.user_id,
-            password: enPw,
-            nickName: req.body.nickName,
-            gender: req.body.gender,
-            location: req.body.location,
-            user_Photo: req.body.user_photo,
-        })
-
-        res.status(210).send("signup ok")
-    }
-}) //인증 - App.js
+//인증 - App.js
 // app.get("/auth", controllers.auth) //인증 - App.js
 
 app.get("/bookmark", controllers.bookmark) //북마크 보는 곳 - BookMark.js
