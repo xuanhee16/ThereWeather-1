a8c42f96b6ad5b66faea33cdb89f2b31351f937d
emily-hong <emilyhong4659@gmail.com>
2021-11-03 10:06:33 +0900

    client/pages모바일레이아웃,스크롤버튼추가

--- client/src/components/GoBackButton.js
+++ client/src/components/GoBackButton.js
@@ -2,30 +2,41 @@
 import styled from "styled-components"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faAngleDoubleLeft } from "@fortawesome/free-solid-svg-icons"
+import { useHistory } from "react-router-dom"
 
 const Button = styled.button`
     display: none;
-
+    
     @media screen and (max-width: 1081px) {
         display: inline;
         /* top: 90vh; */
         top: 1.5vh;
         position: fixed;
         border-radius: 50%;
-
+        z-index:100;
         .fa-fw {
-            font-size: 80px;
+            font-size: 55px;
+        }
+    }
+    @media screen and (max-width: 375px) {
+        .fa-fw {
+            font-size: 40px;
         }
     }
 `
 
 export default function GoBackButton() {
+    const history = useHistory()
+    const goBackHandler = () => {
+        history.goBack();
+    }
     return (
         <Button>
             <FontAwesomeIcon
                 icon={faAngleDoubleLeft}
                 className="fa-fw"
                 color="#ACB5BD"
+                onClick={goBackHandler}
             />
         </Button>
     )
