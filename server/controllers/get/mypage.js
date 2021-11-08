0556b6456d3e1b32280053076bdab66a4f05ba96
seonhwa oh <seonhwa_0713@naver.com>
2021-11-08 13:38:07 +0900

    Fix:Home.js 날씨목록 추가, Write.js 글쓰기 후 마이페이지로 이동

--- server/controllers/get/mypage.js
+++ server/controllers/get/mypage.js
@@ -1,8 +1,11 @@
 const { post } = require("../../models")
 
-module.exports = async (req, res) => {
+module.exports = async (req, res) => { 
+    //res.send()
+
     // const a = await post.findAll({ limit : 3 })
     // console.log(a);
-    console.log(req.query)
-    res.send(await post.findAll({ limit : 6 , order :  [['createdAt', 'DESC']]}))
+    //res.send(await post.findAll({ limit : 6 , order :  [['createdAt', 'DESC']]}))
+    
+    res.send(await post.findAll({}))
 }
