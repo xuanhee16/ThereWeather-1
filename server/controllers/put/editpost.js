10cfdef83dddd48f439e0e49d1a8b99638b0d903
seonhwa oh <seonhwa_0713@naver.com>
2021-11-09 21:53:51 +0900

    Feat:게시물수정 완료

--- server/controllers/put/editpost.js
+++ server/controllers/put/editpost.js
@@ -4,16 +4,14 @@ const { post } = require("../../models")
 module.exports = async (req, res) => {
     //res.send()
     console.log("server/editpost.js",req.body)
-    const { user_id, post_photo, post_title, post_content, weather, wind, temp, top_id, bottom_id } = req.body;
-    // if(req.file){
-    //     res.send({ fileName: req.file.filename })
-    // }
-    // 
-    if(!post_content || !post_content || !weather || !wind || !temp){
+    const { user_id, post_id, post_photo, post_title, post_content, weather, wind, temp } = req.body;
+  
+    if(!post_photo || !post_content || !post_content || !weather || !wind || !temp){
         res.status(401).send("제목,내용, 날씨 정보 등은 필수로 수정해주세요")
     }
     else{
         await post.update({
+            post_photo: post_photo,
             post_title: post_title,
             post_content: post_content,
             weather: weather,
@@ -21,7 +19,8 @@ module.exports = async (req, res) => {
             temp: temp
         }, {
             where: {
-                user_id: user_id,
+                // user_id: user_id,
+                id: req.body.post_id
             }
         })
         res.status(201).send("변경완료")
