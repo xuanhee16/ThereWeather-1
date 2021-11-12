fbf542c1199725d0afd7434f1ba8cc33a004fc98
root <root@DESKTOP-3C4CCR8.localdomain>
2021-11-13 02:21:50 +0900

    배포용 pr5

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ const upload = multer({
 
 //아래 내용 지우지말아주세요
 app.get("/2", (req, res) => {
-    res.send("Hello World!!22!22222")
+    res.send("Hello World!!22!222ss22")
 })
 app.get("/", (req, res) => {
     res.send("Hello World!!ThereWeather!!!!")
