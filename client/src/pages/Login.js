c9bc725026bf82eaa5cde9b3a88f5d129cdfed71
root <root@DESKTOP-3C4CCR8.localdomain>
2021-12-27 15:40:49 +0900

    vercel테스트2

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -96,7 +96,7 @@ const LoginButton = styled.button`
     background-color: pink;
     border-radius: 1rem;
     &:hover {
-        background-color: #FF7F9F;
+        background-color: #ff7f9f;
         color: white;
     }
     > span {
@@ -295,7 +295,8 @@ export default function Login() {
     // input 상태 관리, 유효성 검사
     const [idInput, setIdInput] = useState("")
     const [pwInput, setPwInput] = useState("")
-    const [idInputMessage, setIdInputMessage] = useState("아이디를 입력하세요.")
+    const [idInputMessage, setIdInputMessage] =
+        useState("아이디를 입력하세요..")
     const [pwInputMessage, setPwInputMessage] =
         useState("비밀번호를 입력하세요.")
     const GOOGLE_LOGIN_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=1079927639813-87e5g0991msheh50mt77eclt2vij4kks.apps.googleusercontent.com&response_type=token&redirect_uri=${clientUrl}/login&scope=https://www.googleapis.com/auth/userinfo.email`
