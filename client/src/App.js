85ea1c4a35ddd343f0c45870fecce3a0f19f8619
root <root@DESKTOP-3C4CCR8.localdomain>
2022-01-04 17:33:00 +0900

    환경변수 수정4

--- client/src/App.js
+++ client/src/App.js
@@ -31,12 +31,11 @@ import { changeIsLogin, changeUser } from "./actions/index"
 import styled from "styled-components"
 import GlobalStyle from "./components/GlobalStyle"
 
+let url = process.env.REACT_APP_LOCAL_URL
+if (!url) {
+    url = "https://thereweather.space/api"
+}
 export default function App() {
-    let url = process.env.REACT_APP_LOCAL_URL
-    if (!url) {
-        url = "https://thereweather.space/api"
-    }
-
     const dispatch = useDispatch()
     const history = useHistory()
 
