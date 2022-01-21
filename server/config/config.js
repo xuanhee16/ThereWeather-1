69866b65aa38ae1b0dce056c1bd79536e0a13cf5
root <root@DESKTOP-3C4CCR8.localdomain>
2022-01-21 13:18:21 +0900

    1/21중간pr1

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     development: {
         username: process.env.MYSQL_ID || "root",
         password: process.env.MYSQL_PASSWORD || "1234",
-        database: process.env.MYSQL_NAME || "there_weather",
+        database: process.env.MYSQL_NAME || "TWD",
         host: "127.0.0.1",
         dialect: "mysql",
         timezone: "+09:00",
