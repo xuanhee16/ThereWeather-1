542da99525904831607bd3a927177bd8af985542
seonhwa oh <seonhwa_0713@naver.com>
2022-02-13 13:05:07 +0900

    Chore:server/getAddress.js 주석제거

--- server/controllers/get/geocoder/getAddress.js
+++ server/controllers/get/geocoder/getAddress.js
@@ -8,7 +8,7 @@ module.exports = async function (xLocation, yLocation) {
       url: `http://api.vworld.kr/req/address?service=address&request=getAddress&version=2.0&crs=epsg:4326&point=${yLocation},${xLocation}&format=json&type=both&zipcode=false&simple=true&key=${process.env.GEOCODER_AUTH_KEY}`,
     })
       .then((res) => {
-        console.log("get_address", res.data.response.result);
+        // console.log("get_address", res.data.response.result);
         let addressObj = {};
         if (!res.data.response.result[1]) {
           addressObj = res.data.response.result[0].structure;
