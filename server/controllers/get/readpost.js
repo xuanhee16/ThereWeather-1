0f14c03c36f94a96fee9a574d6adca4541c517ad
seonhwa oh <seonhwa_0713@naver.com>
2022-02-13 12:54:40 +0900

    Chore:server/readpost.js 주석제거

--- server/controllers/get/readpost.js
+++ server/controllers/get/readpost.js
@@ -9,22 +9,22 @@ const getAddress = require("./geocoder/getAddress");
 */
 
 module.exports = async function (req, res) {
-  console.log("**readpost 쿼리 확인**", req.query.id);
+  // console.log("**readpost 쿼리 확인**", req.query.id);
 
   post
     .findOne({ where: { id: req.query.id } })
     .then((raw) => raw.dataValues)
     .then((postData) => {
-      console.log("postData.user_id =" + postData.user_id);
+      // console.log("postData.user_id =" + postData.user_id);
       const userId = postData.user_id;
-      console.log("userId=" + userId);
+      // console.log("userId=" + userId);
 
       // 포스트 데이터에서 작성자 아이디 뽑아내기
       user
         .findOne({ where: { user_id: userId } })
         // 작성자 아이디로 회원 정보 찾기
         .then(async (userinfo) => {
-          console.log("userinfo=" + userinfo);
+          // console.log("userinfo=" + userinfo);
 
           const { nickName, user_Photo } = userinfo.dataValues;
           // 위도 경도 이용해서 주소 찾기
@@ -34,9 +34,9 @@ module.exports = async function (req, res) {
             yLocation
           );
           let address = `${level1} ${level2} ${level4L}`;
-          console.log(level1);
-          console.log(level2);
-          console.log(level4L);
+          // console.log(level1);
+          // console.log(level2);
+          // console.log(level4L);
           if (!level1 || !level2 || !level4L) {
             address = "주소를 찾을 수 없습니다~";
           }
